5d4bea89a67baf1c0d7905067ba0162e0bf47867
olwesel <162628852+olwesel@users.noreply.github.com>
2024-04-29 10:10:48 -0400

    Delete CommandLineGame directory

--- CommandLineGame/command_line_game/Battery.py
+++
@@ -1,7 +0,0 @@
-class Battery(object):
-	def __init__(self, battery):
-		self.battery = battery
-	def changeBattery(self):
-		self.battery = self.battery - 1 #lowers battery by 1
-		return self.battery
-#the battery class is not a scene because the user doesn't enter it - it just contains a method to change the battery which is accessed by the engine class
