87f55a50767e3c9ed32dc887fdccc33c19796dee
olwesel <162628852+olwesel@users.noreply.github.com>
2024-04-29 10:12:53 -0400

    Delete Game.py

--- Game.py
+++
@@ -1,7 +0,0 @@
-import Map
-import Engine
-
-a_map = Map.Map('users/grandpa/desktop') #inputs first scene - the path of the start directory
-a_game = Engine.Engine(a_map) #starts a new game by creating a new instance of the engine class
-a_game.play(100) #sets battery to 100 - so technically if you wanted to change the difficulty level of the game, you could start with a different battery level
-#this is the file the user calls to play the game
